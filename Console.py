18369ec4eaa6b914fd459d8fa12a5cda9b37e73d
andresR0410 <60222767+andresR0410@users.noreply.github.com>
2020-05-11 12:01:36 -0500

    Add files via upload
    Versión inicial de consola y WaveGenerator

---
+++ Console.py
@@ -0,0 +1,3 @@
+"""Interacción con usuario, pide parámetros para la generación de la señal del ECG, llama las funciones de WaveGenerator
+y retorna el resultado."""
+import WaveGenerator as WG
---
+++ WaveGenerator.py
@@ -0,0 +1,4 @@
+"""Se definen todas las funciones para generar la señal del ECG bajo parámetros que se dan en Console por el usuario.
+También se definen funciones para la visualización de los datos y resultados."""
+import numpy as np
+import matplotlib.pyplot as plt
