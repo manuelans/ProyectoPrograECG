fbf570d3a750b59d5358e56489cf7103ab3bde9d
manuelans <63916089+manuelans@users.noreply.github.com>
2020-05-14 00:31:18 -0500

    Update Console.py

--- Console.py
+++ Console.py
@@ -1,3 +1,31 @@
 """Interacción con usuario, pide parámetros para la generación de la señal del ECG, llama las funciones de WaveGenerator
 y retorna el resultado."""
 import WaveGenerator as WG
+
+
+import tkinter as tk
+window= tk.Tk()
+
+pf= tk.Label(
+	text= "Bienvenido a nuestro proyecto EGG"
+	foreground="white",  # Set the text color to white
+    	background="black"  # Set the background color to black
+)
+pf.pack()
+
+def salir():
+    window.destroy()
+
+
+button = tk.Button(
+    master= window,
+    text="salir",
+    width=5,
+    height=5,
+    bg="white",
+    fg="red",
+    command= salir,
+)
+window.mainloop()
+
+
